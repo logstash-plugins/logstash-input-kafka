b5cc613f1fe62be94069ac54342a594f8e6b87ba
Joseph Lawson <joe@joekiller.com>
2014-09-22 21:32:29 -0400

    updated for jruby-kafka 0.2.1, style and documentation
      * style fixes
      * added more documentation to Kafka input

--- Rakefile
+++ Rakefile
@@ -1,12 +1,12 @@
-require "gem_publisher"
+require 'gem_publisher'
 
-desc "Publish gem to RubyGems.org"
+desc 'Publish gem to RubyGems.org'
 task :publish_gem do |t|
-  gem = GemPublisher.publish_if_updated("logstash-input-kafka.gemspec", :rubygems)
+  gem = GemPublisher.publish_if_updated('logstash-input-kafka.gemspec', :rubygems)
   puts "Published #{gem}" if gem
 end
 
 task :default do
-  system("rake -T")
+  system('rake -T')
 end
 
--- spec/inputs/kafka.rb
+++ spec/inputs/kafka.rb
@@ -9,22 +9,22 @@ require 'logstash/errors'
 describe LogStash::Inputs::Kafka do
   extend LogStash::RSpec
 
-  let (:kafka_config) {{"topic_id" => "test"}}
+  let (:kafka_config) {{:topic_id => 'test'}}
 
   it 'should populate kafka config with default values' do
     kafka = LogStash::Inputs::Kafka.new(kafka_config)
-    insist {kafka.zk_connect} == "localhost:2181"
-    insist {kafka.topic_id} == "test"
-    insist {kafka.group_id} == "logstash"
-    insist {kafka.reset_beginning} == false
+    insist {kafka.zk_connect} == 'localhost:2181'
+    insist {kafka.topic_id} == 'test'
+    insist {kafka.group_id} == 'logstash'
+    !insist { kafka.reset_beginning }
   end
 
-  it "should register and load kafka jars without errors" do
+  it 'should register and load kafka jars without errors' do
     kafka = LogStash::Inputs::Kafka.new(kafka_config)
     kafka.register
   end
 
-  it "should retrieve event from kafka" do
+  it 'should retrieve event from kafka' do
     # Extend class to control behavior
     class LogStash::Inputs::TestKafka < LogStash::Inputs::Kafka
       milestone 1
@@ -41,17 +41,17 @@ describe LogStash::Inputs::Kafka do
 
     class Kafka::Group
       public
-      def run(a_numThreads, a_queue)
-        a_queue << "Kafka message"
+      def run(a_num_threads, a_queue)
+        a_queue << 'Kafka message'
       end
     end
 
     logstash_queue = Queue.new
     kafka.run logstash_queue
     e = logstash_queue.pop
-    insist { e["message"] } == "Kafka message"
+    insist { e['message'] } == 'Kafka message'
     # no metadata by default
-    insist { e["kafka"] } == nil
+    insist { e['kafka'] } == nil
   end
 
 end
