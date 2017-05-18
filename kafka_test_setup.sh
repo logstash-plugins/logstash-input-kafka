7dc06e9fccf495797c8a3a614a499b3b6ab0795e
Armin <me@obrown.io>
2017-05-18 10:38:18 +0200

    #118 Fix Flaky IT Setup
    Fixes #197

--- kafka_test_setup.sh
+++ kafka_test_setup.sh
@@ -8,29 +8,28 @@ else
    KAFKA_VERSION=0.10.2.1
 fi
 
+export _JAVA_OPTIONS="-Djava.net.preferIPv4Stack=true"
+
+rm -rf build
+mkdir build
+
 echo "Downloading Kafka version $KAFKA_VERSION"
-curl -s -o kafka.tgz "http://ftp.wayne.edu/apache/kafka/$KAFKA_VERSION/kafka_2.11-$KAFKA_VERSION.tgz"
-mkdir kafka && tar xzf kafka.tgz -C kafka --strip-components 1
+curl -s -o build/kafka.tgz "http://ftp.wayne.edu/apache/kafka/$KAFKA_VERSION/kafka_2.11-$KAFKA_VERSION.tgz"
+mkdir build/kafka && tar xzf build/kafka.tgz -C build/kafka --strip-components 1
 
 echo "Starting ZooKeeper"
-kafka/bin/zookeeper-server-start.sh -daemon kafka/config/zookeeper.properties
+build/kafka/bin/zookeeper-server-start.sh -daemon build/kafka/config/zookeeper.properties
 sleep 10
 echo "Starting Kafka broker"
-kafka/bin/kafka-server-start.sh -daemon kafka/config/server.properties
+build/kafka/bin/kafka-server-start.sh -daemon build/kafka/config/server.properties --override advertised.host.name=127.0.0.1 --override log.dirs="${PWD}/build/kafka-logs"
 sleep 10
 
 echo "Setting up test topics with test data"
-kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_plain --zookeeper localhost:2181
-sleep 10
-kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_snappy --zookeeper localhost:2181
-sleep 10
-kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_lz4 --zookeeper localhost:2181
-sleep 10
-wget https://s3.amazonaws.com/data.elasticsearch.org/apache_logs/apache_logs.txt
-cat apache_logs.txt | kafka/bin/kafka-console-producer.sh --topic logstash_topic_plain --broker-list localhost:9092
-sleep 10
-cat apache_logs.txt | kafka/bin/kafka-console-producer.sh --topic logstash_topic_snappy --broker-list localhost:9092 --compression-codec snappy
-sleep 10
-cat apache_logs.txt | kafka/bin/kafka-console-producer.sh --topic logstash_topic_lz4 --broker-list localhost:9092 --compression-codec lz4
-sleep 10
+build/kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_plain --zookeeper localhost:2181
+build/kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_snappy --zookeeper localhost:2181
+build/kafka/bin/kafka-topics.sh --create --partitions 3 --replication-factor 1 --topic logstash_topic_lz4 --zookeeper localhost:2181
+curl -s -o build/apache_logs.txt https://s3.amazonaws.com/data.elasticsearch.org/apache_logs/apache_logs.txt
+cat build/apache_logs.txt | build/kafka/bin/kafka-console-producer.sh --topic logstash_topic_plain --broker-list localhost:9092
+cat build/apache_logs.txt | build/kafka/bin/kafka-console-producer.sh --topic logstash_topic_snappy --broker-list localhost:9092 --compression-codec snappy
+cat build/apache_logs.txt | build/kafka/bin/kafka-console-producer.sh --topic logstash_topic_lz4 --broker-list localhost:9092 --compression-codec lz4
 echo "Setup complete, running specs"
--- kafka_test_teardown.sh
+++ kafka_test_teardown.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Setup Kafka and create test topics
-
 set -ex
 
 echo "Stopping Kafka broker"
-kafka/bin/kafka-server-stop.sh
+build/kafka/bin/kafka-server-stop.sh
 echo "Stopping zookeeper"
-kafka/bin/zookeeper-server-stop.sh
+build/kafka/bin/zookeeper-server-stop.sh
